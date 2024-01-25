23c63ed7a6ce5b446cfe294ff36389aa4d15b6a5
Hazel Handrata <71081063+kittyofheaven@users.noreply.github.com>
2024-01-25 09:03:16 +0700

    Initial Commit

---
+++ Face Recognition/Face_RecognitionApp.swift
@@ -0,0 +1,32 @@
+//
+//  Face_RecognitionApp.swift
+//  Face Recognition
+//
+//  Created by Hazel Handrata on 25/01/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Face_RecognitionApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
